ce2d806fb4f34f18a312771c0ee2c8333a0499d3
Olukayode Adamolekun <vimkaf@gmail.com>
2021-11-25 11:08:28 +0100

    Lesson 1 and 2

---
+++ 01.dart
@@ -0,0 +1,12 @@
+/*
+NOTES
+=>  main method is the point of entry for the dart program
+=>  the main method can also evaluate mathematical expressions
+
+*/
+
+void main(){
+  print("Hello world");
+
+  print(3 + 2);
+}
